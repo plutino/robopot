fd6cf8527e7a3adab68c922095cdbfe677dd8bb2
Ke Zhang <ke.zhang@appfolio.com>
2014-02-20 21:25:23 -0800

    kz - skeleton

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Robopot::Application.config.secret_token = 'cf9931d58a29885692217262571949086465be66b47116497c6703a02240c5ee2a8cb90492ebb91a2467e74c8c8a27903464a41ea568638c585841b4403a66f9'
